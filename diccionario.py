346a9b4d339326096bfd9e5d0fd5da446b712327
jds73 <davidsanchez@sanbartolo.edu.co>
2025-09-09 18:33:51 -0500

    Create diccionario.py

---
+++ diccionario.py
@@ -0,0 +1,13 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "SIGMA": "eres bueno en algo",
+            "-1000 DE AURA": "hiciste algo mal"
+            }
+print ("bienvenido al tradutor")
+    word = input("Escribe una palabra que no entiendas: ").upper
+for i in range 5
+    if word in meme_dict.keys():
+        print ("significado",meme_dict[word])
+    else:
+        print ("esta palabra aun no esta disponible")
